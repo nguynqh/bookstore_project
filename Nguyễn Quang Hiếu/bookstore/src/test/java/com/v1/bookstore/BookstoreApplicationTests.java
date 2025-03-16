6eefb49fd90922f5b55f739b0eef98fe4b98df2e
nguynqh <mequanghieu9@gmail.com>
2025-03-16 22:41:48 +0700

    Initialize bookstore project with Spring Boot setup and Maven, add file ERD, document for monolithic

---
+++ Nguyễn Quang Hiếu/bookstore/src/test/java/com/v1/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v1.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
